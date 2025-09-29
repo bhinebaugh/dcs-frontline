c9e1e8d26a6fb9393710d120930297b535b48a91
Kent Hinebaugh <bkh@ids.gs>
2025-09-29 18:34:56 -0400

    WIP draw frontline along edge of controlled zones

--- frontline.lua
+++ frontline.lua
@@ -7,6 +7,14 @@
 --or split the cluster
 -- choose a start zone
 -- add a connected zone (one of closest zones)
+function table.contains(table, el)
+    for _, v in pairs(table) do
+        if v == el then
+            return true
+        end
+    end
+    return false
+end
 
 function isCounterClockwise(p1, p2, p3)
     -- swapped to account for DCS coordinate weirdness
@@ -48,7 +56,7 @@ function findPerimeterZones(zones) --convex hull algo
     local current = leftmost
     
     repeat
-        table.insert(hull, zones[current]) --first point will be westmost
+        table.insert(hull, current) --first point will be westmost
         local next = 1
 
         for i = 2, #zones do
@@ -60,7 +68,7 @@ function findPerimeterZones(zones) --convex hull algo
         current = next
     until current == leftmost --we're back at first point
 
-    return hull
+    return hull --returns table of IDs of zone in controlzones
 end
 
 function findNearbyZones(startzone, zones, distance)
@@ -80,6 +88,19 @@ function findNearbyZones(startzone, zones, distance)
     return nearbyzones
 end
 
+function findZonesNearPoint(startPoint, zones, maxDistance)
+    env.info("### looking for points "..maxDistance.." from "..startPoint.x..", "..startPoint.y)
+    local nearbyzones = {}
+    for i, zone in pairs(zones) do
+        local dist = mist.utils.get2DDist(startPoint, { x = zone.x, y = zone.y })
+        env.info("distance to "..zone.name.." is "..dist)
+        if dist < maxDistance then
+            table.insert(nearbyzones, i)
+        end
+    end
+    return nearbyzones
+end
+
 -- env.info( mist.utils.tableShow(mist.DBs.zonesByName) )
 local controlzones = {}
 for zonename, zoneobj in pairs(mist.DBs.zonesByName) do
@@ -91,28 +112,57 @@ for zonename, zoneobj in pairs(mist.DBs.zonesByName) do
   end
 end
 
-local edgezones = findPerimeterZones(controlzones)
-for i, zone in pairs(edgezones) do
-    mist.marker.drawZone(zone.name, { color = {1,1,1,0.2} })
-    trigger.action.textToAll(-1, 240+i, zone.point, {1,1,0.6,1}, {0,0,0,0}, 20, true, i)
+local perimIds = findPerimeterZones(controlzones)
+env.info("Perim IDs"..mist.utils.tableShow(perimIds))
+env.info("length of table: "..#perimIds)
+for i, zoneId in pairs(perimIds) do
+    mist.marker.drawZone(controlzones[zoneId].name, { color = {1,1,1,0.2} })
+    trigger.action.textToAll(-1, 240+i, controlzones[zoneId].point, {1,1,0.6,1}, {0,0,0,0}, 20, true, "Perim "..i.." / Zone "..perimIds[i].."="..zoneId.." ("..controlzones[zoneId].name..")")
 end
-local firstzone = edgezones[ math.random(1, #edgezones) ]
--- trigger.action.circleToAll(-1, firstzone.zoneId, firstzone.point, firstzone.radius, {0,0,0,0}, {0,0,1,0.2}, 1)
-mist.marker.drawZone(firstzone.name, { fillColor = {0,0.5,1,0.5} })
 
---expand zones from starting point
-local maxstartzones = math.floor(#controlzones/2)
+-- choose a few perimeter points for blue
+-- requirements: adjacent, at least 2, not more than half the total
+-- result will be ids (pos) of zones in controlzones table
+local blueOuterPerimIds = {}
+
+local startElem = math.random(#perimIds)
+local endElem = startElem + math.random(math.floor(#perimIds/2)) -- blue will start with at least 2, up to half the perimeter zones
+env.info("start, end: "..startElem..", "..endElem)
+
+for i = startElem, endElem do
+    local pos = i
+    if i > #perimIds then
+        pos = i - #perimIds
+    end
+    table.insert(blueOuterPerimIds, perimIds[pos])
+    env.info("i: "..i..", pos in perim table: "..pos)
+    env.info("perimeter zone #"..pos.." is value (zone) of "..perimIds[pos]..", which is CZ[_]")
+    mist.marker.drawZone(controlzones[perimIds[pos]].name, { fillColor = {0,0.5,1,0.5} })
+end
+env.info("Blue starts with "..#blueOuterPerimIds.." perimeter zones")
+env.info("They are "..mist.utils.tableShow(blueOuterPerimIds))
 
 findCompassMaxima(controlzones)
 local width = controlzones[eastmost].y - controlzones[westmost].y
 local height = controlzones[northmost].x - controlzones[southmost].x
 local centerpoint = { y = 200, x = controlzones[southmost].point.x+height/2, z = controlzones[westmost].point.z+width/2 }
--- trigger.action.markupToAll(4, -1, 999, centerpoint, {0,0,0,1}, {0,0.5,0,0.2}, 1, true, "center") --works as circle, not as markup
-trigger.action.circleToAll(-1, 1, centerpoint, 350, {0,0,0,0}, {0.2,1,0.1,1}, 1) -- mark center of combat area
-local bluezones = findNearbyZones(firstzone, controlzones, math.min(width, height) * math.random(2,7)/10)
-env.info("found nearby zones: "..#bluezones)
-for i, zone in pairs(bluezones) do
-    mist.marker.drawZone(zone.name, { fillColor = {0,0,1,0.3} })
+trigger.action.circleToAll(-1, 1, centerpoint, 350, {0,0,0,0}, {0.8,0.8,0.1,1}, 1) -- mark center of combat area
+
+--find centroid (averaged point, center of mass) of blue perimeter, and use that to find nearby (non-perimeter) zones to create a blue cluster
+local sumX = 0
+local sumY = 0
+for i, id in pairs(blueOuterPerimIds) do
+    sumX = sumX + controlzones[id].x
+    sumY = sumY + controlzones[id].y
+end
+local ctrPerim = { x = sumX/#blueOuterPerimIds, y = sumY/#blueOuterPerimIds }
+trigger.action.circleToAll(-1, 3, {x = ctrPerim.x, y = 200, z = ctrPerim.y}, 350, {0,0,0,0}, {0.3,1,0.6,1}, 1) -- mark center of blue edge area
+local startDistance = math.min(width, height)/2
+local bluezoneIds = findZonesNearPoint(ctrPerim, controlzones, startDistance)
+env.info("made nearby zones blue: "..#bluezoneIds)
+
+for _, zoneId in pairs(bluezoneIds) do
+    mist.marker.drawZone(controlzones[zoneId].name, { color = {0,0,0,0}, fillColor = {0,0,0.7,0.3} })
 end
 table.insert(bluezones, firstzone)
 
