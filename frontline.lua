41da1780a9ff3cafe84606b2f7c8cb63117ec508
Kent Hinebaugh <bkh@ids.gs>
2025-09-30 20:56:37 -0400

    refactor drawing of frontline

--- frontline.lua
+++ frontline.lua
@@ -1,6 +1,32 @@
 --[[ This is handy for development, so that you don't need to delete and re-add the individual scripts in the ME when you make a change.  These will not be packaged with the .miz, so you shouldn't use this script loader for packaging .miz files for other machines/users.  You'll want to add each script individually with a DO SCRIPT FILE ]]--
 --assert(loadfile("C:\\Users\\Kelvin\\Documents\\code\\RotorOps\\scripts\\RotorOps.lua"))()
 
+local ControlZones = {}
+ControlZones.__index = ControlZones
+
+function ControlZones.new(namedZones)
+    local self = setmetatable({}, ControlZones)
+    if not namedZones then
+        self.allZones = {}      --array of names of zones
+        self.zonesByName = {}   --full zone details indexed by zone name
+    else
+        --self.zonesByName = namedZones
+        --put keys into allZones
+    end
+    self.maxima = nil
+    -- self.maxima = {
+    --     westmost = nil,
+    --     eastmost = nil,
+    --     northmost = nil,
+    --     southmost = nil
+    -- }
+    return self
+end
+
+function ControlZones:getZone(name)
+    return self.zonesByName[name]
+end
+
 --get zones whose names start with 'control'
 --get their coordinates
 --grow from opposing start points somehow
@@ -15,6 +41,14 @@ function table.contains(table, el)
     end
     return false
 end
+function table.findIndex(table, needle)
+    for i, v in pairs(table) do
+        if v == needle then
+            return i
+        end
+    end
+    return nil
+end
 
 function isCounterClockwise(p1, p2, p3)
     -- swapped to account for DCS coordinate weirdness
@@ -23,27 +57,37 @@ function isCounterClockwise(p1, p2, p3)
     -- return (p2.x - p1.x) * (p3.y - p1.y) - (p2.y - p1.y) * (p3.x - p1.x) > 0
 end
 
-local westmost = 1 --zone y is E/W yes? == Vec3's z
-local eastmost = 1
-local northmost = 1
-local southmost = 1
-function findCompassMaxima(zones)
-    for i = 2, #zones do
-        if zones[i].y < zones[westmost].y then
-            westmost = i
-        elseif zones[i].y > zones[eastmost].y then
-            eastmost = i
+function ControlZones:assignCompassMaxima()
+    local firstZone = self:getZone(self.allZones[1])
+    self.maxima = {
+        westmost = { name = firstZone.name, x = firstZone.x, y = firstZone.y},
+        eastmost = { name = firstZone.name, x = firstZone.x, y = firstZone.y},
+        southmost = { name = firstZone.name, x = firstZone.x, y = firstZone.y},
+        northmost = { name = firstZone.name, x = firstZone.x, y = firstZone.y},
+    }
+    for i = 2, #self.allZones do
+        local zone = self:getZone(self.allZones[i])
+        if zone.y < self.maxima.westmost.y then
+            self.maxima.westmost = { name = zone.name, x = zone.x, y = zone.y}
+        elseif zone.y > self.maxima.eastmost.y then
+            self.maxima.eastmost = { name = zone.name, x = zone.x, y = zone.y}
         end
-        if zones[i].x < zones[southmost].x then
-            southmost = i
-        elseif zones[i].x > zones[northmost].x then
-            northmost = i
+        if zone.x < self.maxima.southmost.x then
+            self.maxima.southmost = { name = zone.name, x = zone.x, y = zone.y}
+        elseif zone.x > self.maxima.northmost.x then
+            self.maxima.northmost = { name = zone.name, x = zone.x, y = zone.y}
         end
     end
 end
 
-function findPerimeterZones(zones) --convex hull algo
-    if #zones < 3 then return zones end
+function ControlZones:findPerimeter(zoneList) --zoneList is array of indices = names from zonesByName table
+    if #zoneList < 3 then return zoneList end
+
+    local zones = {}
+    for _, zonename in pairs(zoneList) do
+        zone = self:getZone(zonename)
+        table.insert(zones, { name = zonename, x = zone.x, y = zone.y })
+    end
 
     local leftmost = 1
     for i = 2, #zones do
@@ -54,9 +98,9 @@ function findPerimeterZones(zones) --convex hull algo
 
     local hull = {}
     local current = leftmost
-    
+
     repeat
-        table.insert(hull, current) --first point will be westmost
+        table.insert(hull, zones[current].name) --first point will be westmost
         local next = 1
 
         for i = 2, #zones do
@@ -68,7 +112,7 @@ function findPerimeterZones(zones) --convex hull algo
         current = next
     until current == leftmost --we're back at first point
 
-    return hull --returns table of IDs of zone in controlzones
+    return hull --return array of zone names
 end
 
 function findNearbyZones(startzone, zones, distance)
@@ -88,36 +132,38 @@ function findNearbyZones(startzone, zones, distance)
     return nearbyzones
 end
 
-function findZonesNearPoint(startPoint, zones, maxDistance)
+function ControlZones:findZonesNearPoint(startPoint, zoneNames, maxDistance)
     env.info("### looking for points "..maxDistance.." from "..startPoint.x..", "..startPoint.y)
     local nearbyzones = {}
-    for i, zone in pairs(zones) do
+    for i, name in pairs(zoneNames) do
+        local zone = self:getZone(name)
         local dist = mist.utils.get2DDist(startPoint, { x = zone.x, y = zone.y })
         env.info("distance to "..zone.name.." is "..dist)
         if dist < maxDistance then
-            table.insert(nearbyzones, i)
+            table.insert(nearbyzones, name)
         end
     end
     return nearbyzones
 end
 
--- env.info( mist.utils.tableShow(mist.DBs.zonesByName) )
-local controlzones = {}
+local cz = ControlZones.new()
 for zonename, zoneobj in pairs(mist.DBs.zonesByName) do
   if string.sub(zonename,1,7) == 'control' then
     -- add an 'owner' property to zones
     -- zoneobj.owner = 0
-    table.insert(controlzones, zoneobj)
+    cz.zonesByName[zonename] = zoneobj --indexed by name of the trigger zone
+    table.insert(cz.allZones, zoneobj.name) --list of all zone names / names of all zones
     trigger.action.circleToAll(-1, 5000+zoneobj.zoneId, zoneobj.point, 700, {0,0,0,0.5}, {0,0,0,0}, 1)
   end
 end
 
-local perimIds = findPerimeterZones(controlzones)
+local perimIds = cz:findPerimeter(cz.allZones)
 env.info("Perim IDs"..mist.utils.tableShow(perimIds))
 env.info("length of table: "..#perimIds)
 for i, zoneId in pairs(perimIds) do
-    mist.marker.drawZone(controlzones[zoneId].name, { color = {1,1,1,0.2} })
-    trigger.action.textToAll(-1, 240+i, controlzones[zoneId].point, {1,1,0.6,1}, {0,0,0,0}, 20, true, "Perim "..i.." / Zone "..perimIds[i].."="..zoneId.." ("..controlzones[zoneId].name..")")
+    local zone = cz:getZone(zoneId)
+    mist.marker.drawZone(zoneId, { color = {1,1,1,0.2} })
+    trigger.action.textToAll(-1, 240+i, zone.point, {1,1,0.6,1}, {0,0,0,0}, 14, true, "Perim "..i.." / Zone "..perimIds[i].."="..zoneId.." ("..cz.zonesByName[zoneId].name..")")
 end
 
 -- choose a few perimeter points for blue
@@ -137,45 +183,84 @@ for i = startElem, endElem do
     table.insert(blueOuterPerimIds, perimIds[pos])
     env.info("i: "..i..", pos in perim table: "..pos)
     env.info("perimeter zone #"..pos.." is value (zone) of "..perimIds[pos]..", which is CZ[_]")
-    mist.marker.drawZone(controlzones[perimIds[pos]].name, { fillColor = {0,0.5,1,0.5} })
+    mist.marker.drawZone(perimIds[pos], { fillColor = {0,0.5,1,0.5} })
 end
 env.info("Blue starts with "..#blueOuterPerimIds.." perimeter zones")
 env.info("They are "..mist.utils.tableShow(blueOuterPerimIds))
 
-findCompassMaxima(controlzones)
-local width = controlzones[eastmost].y - controlzones[westmost].y
-local height = controlzones[northmost].x - controlzones[southmost].x
-local centerpoint = { y = 200, x = controlzones[southmost].point.x+height/2, z = controlzones[westmost].point.z+width/2 }
+cz:assignCompassMaxima()
+local width = cz.maxima.eastmost.y - cz.maxima.westmost.y
+local height = cz.maxima.northmost.x - cz.maxima.southmost.x
+local centerpoint = { y = 200, x = cz.maxima.southmost.x+height/2, z = cz.maxima.westmost.y+width/2 }
 trigger.action.circleToAll(-1, 1, centerpoint, 350, {0,0,0,0}, {0.8,0.8,0.1,1}, 1) -- mark center of combat area
 
 --find centroid (averaged point, center of mass) of blue perimeter, and use that to find nearby (non-perimeter) zones to create a blue cluster
+--this method could stand to be replaced with something better
 local sumX = 0
 local sumY = 0
 for i, id in pairs(blueOuterPerimIds) do
-    sumX = sumX + controlzones[id].x
-    sumY = sumY + controlzones[id].y
+    sumX = sumX + cz.zonesByName[id].x
+    sumY = sumY + cz.zonesByName[id].y
 end
 local ctrPerim = { x = sumX/#blueOuterPerimIds, y = sumY/#blueOuterPerimIds }
 trigger.action.circleToAll(-1, 3, {x = ctrPerim.x, y = 200, z = ctrPerim.y}, 350, {0,0,0,0}, {0.3,1,0.6,1}, 1) -- mark center of blue edge area
 local startDistance = math.min(width, height)/2
-local bluezoneIds = findZonesNearPoint(ctrPerim, controlzones, startDistance)
+local bluezoneIds = cz:findZonesNearPoint(ctrPerim, cz.allZones, startDistance)
+-- make sure all previously chosen blue outer perim points are included
+for _, name in pairs(blueOuterPerimIds) do
+    if not table.contains(bluezoneIds, name) then
+        table.insert(bluezoneIds, name)
+    end
+end
 env.info("made nearby zones blue: "..#bluezoneIds)
 
 for _, zoneId in pairs(bluezoneIds) do
-    mist.marker.drawZone(controlzones[zoneId].name, { color = {0,0,0,0}, fillColor = {0,0,0.7,0.3} })
+    mist.marker.drawZone(zoneId, { color = {0,0,0,0}, fillColor = {0,0,0.7,0.3} })
 end
-table.insert(bluezones, firstzone)
 
--- map drawing args: shape, coalition, id, pt1...ptN, linecolor, fillcolor, linetype 
--- trigger.action.markupToAll(7, -1, 99, pt1, pt2, pt3, pt4, {1,0,0,1}, {0,0,0,0}, 6)
-local blueedge = findPerimeterZones(bluezones)
-local initialPt = blueedge[1].point
-local ptA = initialPt
-for i = 2, #blueedge do
-    ptB = blueedge[i].point
-    trigger.action.markupToAll(7, -1, 700+i, ptA, ptB, {0,0,1,1}, {0,0,1,1}, 4)
+--remove points that were elements of the whole perimeter
+--so only 'internal' perimeter remains, i.e. points facing non-blue points
+local bluePerimIds = cz:findPerimeter(bluezoneIds)
+env.info(#bluePerimIds.." zones form the blue perimeter: "..mist.utils.tableShow(bluePerimIds))
+env.info("battlefront anchor zones "..blueOuterPerimIds[1]..", "..blueOuterPerimIds[#blueOuterPerimIds])
+
+for i, zoneId in pairs(bluePerimIds) do
+    local pt = mist.projectPoint(cz:getZone(zoneId).point, 500, math.rad(180))
+    mist.marker.drawZone(zoneId, { color = {0.1,0,1,1} })
+    trigger.action.textToAll(-1, 140+i, pt, {0,0,0,0.5}, {0,0,0,0}, 14, true, "Blue Perim "..i.." / Zone "..zoneId)
+end
+
+--create a sequence of line segments that connect blue zones between first and last bluePerimIds
+--we have 3 sequences of points:
+--perimIds, which is the outer polygon
+--blueOuterPerimIds, which is the sequence within perimIds that is blue
+--bluePerimIds, which is blueOuterPerimIds plus points not on outer polygon (ie not a member of perimIds)
+
+local frontlinePoints = {}
+-- start with last outer perim point (b/c both perims are constructed as points listed ccw)
+-- then continue adding from blue perim list  until first outer perim point reached, wrapping around to start of array 
+local lastSharedPerimeterId = blueOuterPerimIds[#blueOuterPerimIds]
+local frontlineStartIndex = table.findIndex(bluePerimIds, lastSharedPerimeterId)
+local p = frontlineStartIndex
+for i = 1, #bluePerimIds do
+    table.insert(frontlinePoints, bluePerimIds[p])
+    if bluePerimIds[p] == blueOuterPerimIds[1] then
+        break
+    end
+    p = p + 1
+    if p > #bluePerimIds then p = 1 end
+end
+
+env.info("Frontline points (inclusive) "..mist.utils.tableShow(frontlinePoints))
+
+local ptA = cz:getZone(frontlinePoints[1]).point
+env.info("line segment from "..frontlinePoints[1])
+local ptB
+for i = 2, #frontlinePoints do
+    ptB = cz:getZone(frontlinePoints[i]).point
+    trigger.action.markupToAll(7, -1, 1700+i, ptA, ptB, {0,0,1,1}, {0,0,1,1}, 4)
+    env.info("line segment from "..frontlinePoints[i])
     -- mist.marker.add({ pos = ptB, id = 800+i, markType = 5, message = "Line "..blueedge[i].name })
     -- trigger.action.textToAll(-1, 800+i, ptB, {0,0,0,1}, {0,0,0,0}, 20, true, "Line "..blueedge[i].name.." to "..blueedge[i-1].name)
     ptA = ptB
 end
-trigger.action.markupToAll(7, -1, 777, initialPt, ptB, {0,0,1,1}, {0,0,0,1}, 4)
